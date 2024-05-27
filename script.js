6e58c1334be528872d44a4ff09b072ef64444280
Rizky Saputra <muhamadrizkysaputra34@gmail.com>
2024-05-27 16:39:51 +0700

    Update level

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var p, hasil, repeat = true;
 
-alert('Halooo mbuuubbb, niii bubupp bikin game gabuttt mainin dehhh...');
-alert('Selamat Bermain sayangkuuuu');
-
 while ( repeat ){
 
-var level = prompt('Pilih level :\n1. Easy\n2. Medium\n3. Hard');
+var level = prompt('Pilih level :\n1. Easy\n2. Medium\n3. Hard\n4. Very Hard\n5. IMPOSSIBLE');
 
 switch ( level ){
+    case '5' :
+        var x = Math.floor(Math.random() * 1000), inGame = 1, nyawa = 10;
+        alert('Tebak angka 1 - 1000\nKesempatan : ' + nyawa);
+        break;
+    case '4' :
+        var x = Math.floor(Math.random() * 500), inGame = 1, nyawa = 10;
+        alert('Tebak angka 1 - 500\nKesempatan : ' + nyawa);
+        break;
     case '3' :
         var x = Math.floor(Math.random() * 100), inGame = 1, nyawa = 10;
         alert('Tebak angka 1 - 100\nKesempatan : ' + nyawa);
@@ -25,7 +30,7 @@ switch ( level ){
 }
 
 while ( nyawa > 0 && inGame == 1) {
-    p = prompt('Masukkan angka!\nKesempatan : ' + nyawa);
+    p = prompt('Masukkan angka!\nKesempatan : ' + nyawa + '\nketik "n" jika menyerah');
         if ( p == x ) {
             hasil = 'Angka yang di cari = ' + x + '\nKamu Benar!';
             inGame = 0;
@@ -35,6 +40,9 @@ while ( nyawa > 0 && inGame == 1) {
         } else if ( p > x ) {
             hasil = 'Terlalu tinggi!';
             nyawa -= 1;
+        } else if ( p == 'n' || p == 'N' ){
+            hasil = 'Cupuuu..';
+            inGame = 0;
         } else {
             alert('Yang anda masukkan salah!')
         }
